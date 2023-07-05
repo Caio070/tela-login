eeb9eff1edba19ee1c9c63999a71f5789e4a3aad
Perkles <caioln.077@gmail.com>
2023-07-05 17:54:53 -0300

    Alterações e adição do JS

---
+++ js/main.js
@@ -0,0 +1,61 @@
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function checkInputs() {
+  const emailValue = email.value;
+  const passwordValue = password.value;
+
+  if (emailValue === "") {
+    setErrorFor(email, "O e-mail é obrigatório.");
+  } else if (!checkEmail(emailValue)) {
+    setErrorFor(email, "Por favor, insira um email válido.");
+  } else {
+    setSuccesFor(email);
+  }
+
+  if (passwordValue === "") {
+    setErrorFor(password, "A senha é obrigatória.");
+  } else if (passwordValue.length < 7) {
+    setErrorFor(password, "A senha precisa ter no mínimo 7 carácteres.");
+  } else {
+    setSuccesFor(password);
+  }
+
+  const formControls = form.querySelectorAll(".form-control");
+
+  const formIsValid = [...formControls].every((formControl) => {
+    return formControl.className === "form-control success";
+  });
+
+  if (formIsValid) {
+    console.log("O formulário é válido.");
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  small.innerText = message;
+
+  formControl.className = "form-control error";
+}
+
+function setSuccesFor(input) {
+  const formControl = input.parentElement;
+
+  formControl.className = "form-control success";
+}
+
+function checkEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
